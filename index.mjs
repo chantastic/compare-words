2241c484a27366677e1505599fa1b17b500e29e5
Michael Chan <mijoch@gmail.com>
2022-01-15 14:22:02 -0800

    setup expand function

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
-export function validate(lh, rh) {
-  if (lh === rh) {
-    return true;
+// correct | present | absent
+
+export function expand(word) {
+  if (typeof word !== "string") {
+    throw new TypeError("Guess must be a string.");
   }
-  return false;
+
+  return word.split("").map((letter) => [letter, undefined]);
 }
--- test.mjs
+++ test.mjs
@@ -1,6 +1,23 @@
-import { validate } from "./index.mjs";
+import { expand } from "./index.mjs";
 import test from "ava";
 
-test("Guess matches answer", async (t) => {
-  t.is(validate("cigar", "cigar"), true);
+test("expand - throws if input word is not string", (t) => {
+  const error = t.throws(
+    () => {
+      expand();
+    },
+    { instanceOf: TypeError }
+  );
+
+  t.is(error.message, "Guess must be a string.");
+});
+
+test("expand - expands guess to matrix", (t) => {
+  t.deepEqual(expand("clang"), [
+    ["c", undefined],
+    ["l", undefined],
+    ["a", undefined],
+    ["n", undefined],
+    ["g", undefined],
+  ]);
 });
